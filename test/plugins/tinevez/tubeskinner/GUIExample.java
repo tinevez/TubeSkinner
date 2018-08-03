aaf6cce4e0273a8531366c18c1c7eb088db83145
Jean-Yves Tinevez <tinevez@pasteur.fr>
2018-08-03 11:59:13 +0200

    Example showing the EzPlug GUI.

---
+++ test/plugins/tinevez/tubeskinner/GUIExample.java
@@ -0,0 +1,35 @@
+package plugins.tinevez.tubeskinner;
+
+import java.lang.reflect.InvocationTargetException;
+
+import javax.swing.SwingUtilities;
+
+import icy.file.Loader;
+import icy.gui.viewer.Viewer;
+import icy.main.Icy;
+import icy.sequence.Sequence;
+import plugins.kernel.roi.roi2d.ROI2DEllipse;
+
+public class GUIExample
+{
+
+	public static void main( final String[] args ) throws InvocationTargetException, InterruptedException
+	{
+		final String imagePath = "samples/MyleneAorta.tif";
+
+		Icy.main( args );
+		final Sequence sequence = Loader.loadSequence( imagePath, 0, true );
+		SwingUtilities.invokeLater( () -> {
+			final Viewer viewer = new Viewer( sequence );
+			viewer.setVisible( true );
+			viewer.setPositionZ( 0 );
+			sequence.removeAllROI();
+			final ROI2DEllipse circle = new ROI2DEllipse( 63, 46, 147, 130 );
+			sequence.addROI( circle );
+			new TubeSkinnerGUI().showUI();
+		} );
+	}
+
+	private GUIExample()
+	{}
+}
