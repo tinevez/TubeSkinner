8d608c428703badb03d6d56b4ca280a97b4567b3
sebherbert <sherbert@pasteur.fr>
2019-04-26 17:47:19 +0200

    Modified for easier icy launch in eclipse

--- test/plugins/tinevez/tubeskinner/GUIExample.java
+++ test/plugins/tinevez/tubeskinner/GUIExample.java
@@ -7,6 +7,8 @@ import javax.swing.SwingUtilities;
 import icy.file.Loader;
 import icy.gui.viewer.Viewer;
 import icy.main.Icy;
+import icy.plugin.PluginLauncher;
+import icy.plugin.PluginLoader;
 import icy.sequence.Sequence;
 import plugins.kernel.roi.roi2d.ROI2DEllipse;
 
@@ -15,7 +17,7 @@ public class GUIExample
 
 	public static void main( final String[] args ) throws InvocationTargetException, InterruptedException
 	{
-		final String imagePath = "samples/MyleneAorta.tif";
+		final String imagePath = "/media/sherbert/Data/Scripts/eclipse-workspace/TubeSkinner/samples/MyleneAorta.tif";
 
 		Icy.main( args );
 		final Sequence sequence = Loader.loadSequence( imagePath, 0, true );
@@ -26,7 +28,8 @@ public class GUIExample
 			sequence.removeAllROI();
 			final ROI2DEllipse circle = new ROI2DEllipse( 63, 46, 147, 130 );
 			sequence.addROI( circle );
-			new TubeSkinnerGUI().showUI();
+			PluginLauncher.start( PluginLoader.getPlugin( TubeSkinnerGUI.class.getName() ) );
+			// new TubeSkinnerGUI().showUI();
 		} );
 	}
 
